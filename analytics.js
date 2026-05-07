ea9cc048a0ea9a87271f46271ff2a0fb4fddc313
tsukukiku <zhanghaiyanxgs@yahoo.co.jp>
2026-05-07 12:08:46 +0900

    Add files via upload

--- analytics.js
+++ analytics.js
@@ -1,19 +1,21 @@
-// Fill in your Google Analytics measurement ID to enable visitor statistics.
-// Example: window.ZODIAC_GA_ID = "G-XXXXXXXXXX";
-window.ZODIAC_GA_ID = "G-L0376CLDH8";
+window.STAR_STYLE_GA_ID = "G-L0376CLDH8";
 
-if (window.ZODIAC_GA_ID) {
+if (window.STAR_STYLE_GA_ID && !window.__STAR_STYLE_GA_LOADED__) {
+  window.__STAR_STYLE_GA_LOADED__ = true;
+
   const gtagScript = document.createElement("script");
   gtagScript.async = true;
-  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${window.ZODIAC_GA_ID}`;
+  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${window.STAR_STYLE_GA_ID}`;
   document.head.appendChild(gtagScript);
 
   window.dataLayer = window.dataLayer || [];
   function gtag() {
     window.dataLayer.push(arguments);
   }
-  gtag("js", new Date());
-  gtag("config", window.ZODIAC_GA_ID, {
+
+  window.gtag = window.gtag || gtag;
+  window.gtag("js", new Date());
+  window.gtag("config", window.STAR_STYLE_GA_ID, {
     anonymize_ip: true
   });
 }
